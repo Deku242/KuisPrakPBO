072e014e07f1a2b2c4a5b889be46a0f5f13a6bec
Muhammad Ilham Nursastrawan <72774021+Deku242@users.noreply.github.com>
2022-04-04 10:23:59 +0700

    Add files via upload

---
+++ src/kuispbo/Designer.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kuispbo;
+
+/**
+ *
+ * @author Machine Game
+ */
+public class Designer extends Pendaftaran implements Seleksi {
+
+    public Designer( String nama, double nilai1, double nilaiCoding, double nilai3) {
+        super( nama, nilai1, nilaiCoding, nilai3);
+    }
+    
+    @Override
+    public double seleksi1() {
+        return (double) (this.nilai1 * 0.3);
+    }
+
+    @Override
+    public double seleksi2() {
+        return (double) (this.nilai2 * 0.45);
+    }
+
+    @Override
+    public double seleksi3() {
+        return (double) (this.nilai3 * 0.25);
+    }
+    
+}
